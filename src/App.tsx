ed30c17d8fe9c13b73503ecca87a2a34634296af
lucascorreaa <lucas.lca14@gmail.com>
2024-05-11 16:41:51 -0300

    feat: created challenge repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card } from './components/card';
+import { Header } from './components/header';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Products } from './components/products';
+import { Footer } from './components/footer';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Header />
+      <Products />
+      <Footer />
+    </QueryClientProvider>
   );
 }
 
